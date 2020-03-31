d753c985a5ccf8d618c4a95c1a29abd74bda1818
olezhkalviv <oleh.yosyfovych@gmail.com>
2020-04-01 01:08:07 +0300

    Implemented showing list of movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import moviesData from "../moviesData";
 
-function App() {
-  return <div>Hello ReactWarriors!</div>;
+let title = "Hello ReactWarriors";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData
+    }
+  }
+  render() {
+    console.log(this);
+    return <div>{this.state.movies.map(function (movie) {
+      return <p>{movie.title}</p>
+    })}</div>;
+  }
 }
 
 export default App;
