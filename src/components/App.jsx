58d39f348cea66cfab7ed364cd6e91be4fd8e433
olezhkalviv <oleh.yosyfovych@gmail.com>
2020-04-01 04:09:01 +0300

    Implemented MovieTabs component; redesigned logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MovieItem from "./MovieItem"
+import MovieTabs from "./MovieTabs"
 import { API_URL, API_KEY_3 } from "../utils/api"
 
 class App extends React.Component {
@@ -8,15 +9,15 @@ class App extends React.Component {
 
     this.state = {
       movies: [],
-      moviesWillWatch: []
+      moviesWillWatch: [],
+      sort_by: "revenue.desc"
     }
 
     //this.removeMovie = this.removeMovie.bind(this); - if removeMovie is not '=>' function
   }
 
   componentDidMount() {
-    console.log("didMount")
-    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=revenue.desc`).then((response) => {
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`).then((response) => {
       console.log("then");
       return response.json()
     }).then((data) => {
@@ -44,11 +45,22 @@ class App extends React.Component {
     this.setState({ moviesWillWatch: updateWillWatch });
   }
 
+  updateSortBy = value => {
+    this.setState({
+      sort_by: value
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <div className="row">
+        <div className="row mt-4">
           <div className="col-9">
+            <div className="row mb-4">
+              <div className="col-12">
+                <MovieTabs sort_by={this.state.sort_by} updateSortBy={this.updateSortBy} />
+              </div>
+            </div>
             <div className="row">
               {this.state.movies.map((movie) => {
                 return (<div className="col-6 mb-4" key={movie.id}>
