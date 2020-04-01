1175897b1cd5048e32a5fa457d3ee697c3cbb917
olezhkalviv <oleh.yosyfovych@gmail.com>
2020-04-01 03:30:45 +0300

    Imlemented getting list from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import moviesData from "../moviesData";
 import MovieItem from "./MovieItem"
+import { API_URL, API_KEY_3 } from "../utils/api"
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      movies: moviesData,
+      movies: [],
       moviesWillWatch: []
     }
 
     //this.removeMovie = this.removeMovie.bind(this); - if removeMovie is not '=>' function
+  }
+
+  componentDidMount() {
+    console.log("didMount")
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=revenue.desc`).then((response) => {
+      console.log("then");
+      return response.json()
+    }).then((data) => {
+      console.log("then2", data)
+      this.setState({ movies: data.results })
+    })
   }
 
   removeMovie = (movie) => {
