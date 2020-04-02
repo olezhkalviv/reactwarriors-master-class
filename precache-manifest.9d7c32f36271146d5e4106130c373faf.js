02025baac98449d892e41c184aadb882c134d8b5
olezhkalviv <oleh.yosyfovych@gmail.com>
2020-04-03 01:32:55 +0300

    Updates

---
+++ precache-manifest.9d7c32f36271146d5e4106130c373faf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d3f02228a9f8fc93cef54f9f1d8e0c",
+    "url": "/reactwarriors-master-class/index.html"
+  },
+  {
+    "revision": "7c6658cc7c197fe2f441",
+    "url": "/reactwarriors-master-class/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7fe6adec4e5f854a0f27",
+    "url": "/reactwarriors-master-class/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "7c6658cc7c197fe2f441",
+    "url": "/reactwarriors-master-class/static/js/2.57abb275.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactwarriors-master-class/static/js/2.57abb275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe6adec4e5f854a0f27",
+    "url": "/reactwarriors-master-class/static/js/main.620e94eb.chunk.js"
+  },
+  {
+    "revision": "20a0b58614ef357b9771",
+    "url": "/reactwarriors-master-class/static/js/runtime-main.05aedc1f.js"
+  }
+]);
